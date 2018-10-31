e4311dc0bd349e5ddff33b10a0e55ad300d7d5bf
Jason Stone <jason.stone0808@gmail.com>
2018-10-31 02:50:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   else if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else "NO, NOT SINCE 1938!"
 end
